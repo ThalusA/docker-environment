8b07e459a4f416440e319a0f013e45a636400ac8
ThalusA <alexandre.monierc@gmail.com>
2021-12-16 18:13:14 +0100

    Update Dockerfile

--- load.sh
+++ load.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 sudo docker run \
+            --privileged --cap-add \
             -v /var/run/docker.sock:/var/run/docker.sock \
             -v $HOME/.ssh:/root/.ssh \
-            -ti docker-devenv .
+            -ti thalus/devenv .
